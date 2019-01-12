68230a37bb541e6b078fed57c491a5711e9f3aa2
tajri04 <tajri.fouad@gmail.com>
2019-01-12 17:38:04 +0100

    first commit

---
+++ src/app/recipes/recipe-detail/recipe-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-item',
+  templateUrl: './recipe-detail.component.html',
+  styleUrls: ['./recipe-detail.component.css']
+})
+export class RecipeItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
